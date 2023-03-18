1c68bd00e19f1d1b4a57d483e829cb1ce77e9124
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-18 14:45:51 +0200

    Tree View example.

---
+++ 11_tree_list_view/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace _11_tree_list_view
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Personal");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Work");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Documents", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Downloads");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Pictures");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(12, 58);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node4";
+            treeNode1.Text = "Personal";
+            treeNode2.Name = "Node5";
+            treeNode2.Text = "Work";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "Documents";
+            treeNode4.Name = "Node2";
+            treeNode4.Text = "Downloads";
+            treeNode5.Name = "Node3";
+            treeNode5.Text = "Pictures";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(623, 901);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "folder (1).png");
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripProgressBar1,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1881, 42);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(46, 36);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 36);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(173, 36);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(678, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1191, 900);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1881, 971);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TreeView treeView1;
+        private ImageList imageList1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripProgressBar toolStripProgressBar1;
+        private ToolStripLabel toolStripLabel1;
+        private ListBox listBox1;
+    }
+}
---
+++ 11_tree_list_view/Form1.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+
+namespace _11_tree_list_view
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            treeView1.Nodes.Clear();
+
+            // "C:\Users\Vlad"
+            string userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            LoadDirectories(userProfilePath, treeView1.Nodes);
+        }
+
+        private void LoadDirectories(string root, TreeNodeCollection nodes)
+        {
+            try
+            {
+                var dirInfo = new DirectoryInfo(root);
+
+                foreach (var d in dirInfo.GetDirectories())
+                {
+                    // create directory node
+                    TreeNode node = new TreeNode(d.Name);
+                    node.Tag = d.FullName;
+
+                    // recursively load subdirectories
+                    LoadDirectories(d.FullName, node.Nodes);
+
+                    // add node to the collection
+                    nodes.Add(node);
+                }
+            }
+            catch { }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeView1.SelectedNode == null) return;
+
+            var node = treeView1.SelectedNode;
+
+            if (node.Tag == null) return;
+
+            DirectoryInfo dir = new(node.Tag.ToString());
+
+            listBox1.Items.Clear();
+            foreach (var f in dir.GetFiles())
+            {
+                listBox1.Items.Add(f.Name);
+            }
+            
+            // open file: Process.Start(path);
+        }
+    }
+}
